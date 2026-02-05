d53447fb18a669bf0911bd476bd192bda870aa52
nishanth-duraisamy <nishanthduraisamy@gmail.com>
2026-02-05 18:07:54 +0530

    loading removed

--- src/components/BookingRoot.jsx
+++ src/components/BookingRoot.jsx
@@ -1,12 +1,7 @@
-import { LoadingProvider } from '../contexts/LoadingContext';
 import Appointments from './Appointments';
-import GlobalLoader from './GlobalLoader';
 
 export default function BookingRoot() {
     return (
-        <LoadingProvider>
-            <GlobalLoader />
-            <Appointments />
-        </LoadingProvider>
+        <Appointments />
     );
 }
