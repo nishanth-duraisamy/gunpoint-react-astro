c31e4f3e173c34b612997ef26b91f77bada76132
nishanth-duraisamy <nishanthduraisamy@gmail.com>
2026-02-06 00:22:47 +0530

    bug fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import react from "@astrojs/react";
 import tailwindcss from '@tailwindcss/vite';
-import sitemap from '@astrojs/sitemap'; // Import sitemap integration
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://gunpointtattoostudio.com', // Add the site URL for sitemap generation
-  integrations: [react(), sitemap()], // Add sitemap integration
+  site: 'https://gunpointtattoostudio.com',
+  integrations: [react(), sitemap()],
   vite: {
     plugins: [
         tailwindcss()
